3d77924b4c46bbfda5e40ce112c83e99a86257bb
yck <597813039@qq.com>
2017-01-11 17:59:56 +0800

    wx

--- 微信小程序/pages/posts/posts-detail/posts-detail.js
+++ 微信小程序/pages/posts/posts-detail/posts-detail.js
@@ -34,13 +34,13 @@ Page({
       var collected = collections[this.data.id]
       collected = !collected
       collections[this.data.id] = collected
-      wx.setStorageSync("post-collected", collections)
-      this.showModal(collected)
-    //   this.showToast(collected)
+      
+      this.showModal(collected, collections)
+    //   this.showToast(collected, collections)
   },
 
   // 展示模态窗
-  showModal: function(collected) {
+  showModal: function(collected, collections) {
       // 在 success 中作用域改变了，this 去不到不代表 page，所以需要在外面需要将 this 赋值给别的变量
       var that = this
       wx.showModal({
@@ -48,13 +48,13 @@ Page({
         content: collected ? "是否收藏？" : "是否取消收藏？",
         success: function(res) {
             if (res.confirm) {
-                that.showToast(collected)
+                that.showToast(collected, collections)
             }
         }
 })
   },
 
-  showToast: function(collected) {
+  showToast: function(collected, collections) {
       this.setData({
         collected: collected
       })
@@ -64,5 +64,6 @@ Page({
         icon: 'success',
         duration: 1000
       })
+      wx.setStorageSync("post-collected", collections)
   }
 })
