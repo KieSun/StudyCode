669ac2fa90e45465e5fbb117cb3b2b7600418e3b
yuchengkai <zx597813039@gmail.com>
2017-01-22 21:39:56 +0800

    Mit

---
+++ MIT 6.0001/week1.py
@@ -0,0 +1,15 @@
+a = round(2.6)
+# b = "3" * "bc"
+# print b
+c = "helloworld"
+print c[::-1]
+temp = '32'
+if temp > 85:
+   print "Hot"
+elif temp > 62:
+   print "Comfortable"
+else:
+   print "Cold"
+
+if type("123") == str:
+    print "yes"
