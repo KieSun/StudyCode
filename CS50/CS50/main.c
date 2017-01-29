ace3a273d6804fd945f06ce3c8b4757e1d9bc239
yuchengkai <zx597813039@gmail.com>
2017-01-29 21:26:10 +0800

    cs50

---
+++ CS50/CS50/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  CS50
+//
+//  Created by 俞诚恺 on 2017/1/29.
+//  Copyright © 2017年 俞诚恺. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    
+    while (1) {
+        printf("Hello, World!\n");
+    }
+    return 0;
+}
